1ac6cc47c2634d7613c1fc609c7fb6a4017958f9
wulf_ni <niklas.wulff@dlr.de>
2020-03-03 08:59:32 +0100

    Commit logging nicely use with caution and wisely but if you succeed you're able to time your functions precisely

---
+++ scripts/libLogging.py
@@ -0,0 +1,44 @@
+#-*- coding:utf-8 -*-
+
+__version__ = '0.1.0'
+__maintainer__ = 'Benjamin Fuchs 12.11.2019'
+__email__ = 'Benjamin.Fuchs@dlr.de'
+__birthdate__ = '26.07.2019'
+__status__ = 'dev'  # options are: dev, test, prod
+
+
+import functools
+import time
+import inspect
+import logging.handlers
+
+logger = logging.getLogger('debugger')
+h = logging.handlers.RotatingFileHandler('./debug.log')
+logger.addHandler(h)
+h = logging.StreamHandler()
+logger.addHandler(h)
+logger.setLevel('DEBUG')
+
+
+def logit(f):
+
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        logger.debug(f'entering function "{f.__name__}"')
+        src = inspect.getsource(f)
+        logger.debug(f'source:\n{src}')
+        start = time.time()
+        try:
+            ret = f(*args, **kwargs)
+        except Exception as E:
+            logger.exception(f'Error during call of function "{f.__name__}"')
+            raise E
+
+        end = time.time()
+        logger.debug(f'function call took {end-start} ms')
+        logger.debug(f'exiting function "{f.__name__}"')
+        return ret  # added this line myself since variable ret wasn't used before
+
+    return wrapper
+
+
